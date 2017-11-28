3fa8bef2f67138040e5af60e59a5c58bf603feec
bajarang <bajarang.sutar@cern.ch>
2017-11-28 19:04:44 +0530

    Change argument list

--- Includes/DataDrivenQCD.h
+++ Includes/DataDrivenQCD.h
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-void DataDrivenQCD( string leptonFlavor = "SMu", int METcut = 0,  int doBJets = 0);
+void DataDrivenQCD(string leptonFlavor, int charge, int systematics, int direction, int JetPtCutMin, int doBJets, int MET, int mT, string type);
 
-void FuncOpenAllFiles(TFile *fData[], TFile *fMC[][14], string leptonFlavor = "SMu",  int METcut = 0, bool doFlat = false, bool doVarWidth = true , int doBJets = 0, string syst = "0" );
+void FuncOpenAllFiles(TFile *fData[], TFile *fMC[][14], string leptonFlavor,  int charge, int systematics, int direction, int JetPtCutMin, int doBJets, int MET, int mT, string type);
 
 vector<string> getVectorOfHistoNames(TFile *fData[]);
 
