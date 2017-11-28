01dc6f2062e3b3f898c1f35355fdffb2eb6230e9
bajarang <bajarang.sutar@cern.ch>
2017-11-28 18:13:56 +0530

    Comment out unnecessary(as-of-now) functions

--- Includes/getFilesAndHistograms.h
+++ Includes/getFilesAndHistograms.h
@@ -12,7 +12,7 @@ using namespace std;
 
 string getEnergy();
 TFile* getFile(string histoFilesDirectory, string leptonFlavor, string energy, string Name, int charge, int systematics, int direction, int doBJets, int doQCD, int MET, int mT, string type, int JetPtMin = 30, int JetPtMax = 0, bool doFlat = false, bool doVarWidth = false, bool doSSign = false , bool doInvMassCut = false , string closureTest = "", bool useRoch = false, bool dodR = false, bool useUnfoldingFiles = false ); //22 arguments
-void getFiles(string, TFile *Files[], string, string, string, int JetPtMin = 20, int JetPtMax = 0, bool doFlat = false ,  bool doVarWidth = false , int doQCD = 0,  bool doSSign = false , bool doInvMassCut = false, int MET = 0  , int doBJets = 0 , bool useUnfoldingFiles = false );
+////void getFiles(string, TFile *Files[], string, string, string, int JetPtMin = 20, int JetPtMax = 0, bool doFlat = false ,  bool doVarWidth = false , int doQCD = 0,  bool doSSign = false , bool doInvMassCut = false, int MET = 0  , int doBJets = 0 , bool useUnfoldingFiles = false );
 void closeFile(TFile*);
 void closeFiles(TFile *Files[]);
 void closeFiles(TFile *Files[], int nFiles);
